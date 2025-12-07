02a6c6bb87fe5021fd4c875807ea27e9409124de
Franco <franco_tigres18@hotmail.com>
2025-12-07 16:50:06 -0600

    feat:change format files

--- src/models/Budget.ts
+++ src/models/Budget.ts
@@ -11,17 +11,16 @@ import {
 @Table({
   tableName: "budgets",
 })
-
 class Budget extends Model {
-    @Column({
-        type: DataType.STRING(100)
-    })
-    name:string
+  @Column({
+    type: DataType.STRING(100),
+  })
+  name: string;
 
-    @Column({
-        type: DataType.DECIMAL
-    })
-    amount :number
+  @Column({
+    type: DataType.DECIMAL,
+  })
+  amount: number;
 }
 
-export default Budget
+export default Budget;
