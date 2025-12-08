8a7ec534c11d27189c74a9c3912ed79c69ea6e35
Franco <franco_tigres18@hotmail.com>
2025-12-07 18:54:02 -0600

    feat:update a budget

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -28,21 +28,36 @@ export class BudgetController {
   static getById = async (req: Request, res: Response) => {
     try {
       const { id } = req.params;
-      const budget = await Budget.findByPk(id)
+      const budget = await Budget.findByPk(id);
 
-      if(!budget){
-        const error = new Error('Presupuesto no encontrado')
-        return res.status(404).json({error: error.message})
+      if (!budget) {
+        const error = new Error("Presupuesto no encontrado");
+        return res.status(404).json({ error: error.message });
       }
-      
-      res.json(budget)
+
+      res.json(budget);
     } catch (error) {
       res.status(500).json({ error: "Hubo un error" });
     }
   };
 
-  static updateById = (req: Request, res: Response) => {
-    console.log("Desde Update /app/budgets");
+  static updateById = async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const budget = await Budget.findByPk(id);
+
+      if (!budget) {
+        const error = new Error("Presupuesto no encontrado");
+        return res.status(404).json({ error: error.message });
+      }
+
+      //   Escribir los cambios del body
+      await budget.update(req.body);
+
+      res.json("Presupuesto actualizado correctamente");
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error" });
+    }
   };
 
   static deleteById = (req: Request, res: Response) => {
--- src/routes/budget.routes.ts
+++ src/routes/budget.routes.ts
@@ -34,7 +34,26 @@ router.get(
   BudgetController.getById
 );
 
-router.put("/:id", BudgetController.updateById);
+router.put(
+  "/:id",
+  param("id")
+    .isInt()
+    .withMessage("ID no válido")
+    .custom((value) => value > 0)
+    .withMessage("ID no válido"),
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre del presupuesto no puede ir vacío"),
+  body("amount")
+    .notEmpty()
+    .withMessage("La cantidad del presupuesto no puede ir vacía")
+    .isNumeric()
+    .withMessage("Cantidad no válida")
+    .custom((value) => value > 0)
+    .withMessage("El presupuesto debe ser mayor a 0"),
+  handleInputErrors,
+  BudgetController.updateById
+);
 router.delete("/:id", BudgetController.deleteById);
 
 export default router;
