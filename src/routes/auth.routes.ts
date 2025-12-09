138319d33b25b0eafadf4514bf45d3781bb01fdb
Franco <franco_tigres18@hotmail.com>
2025-12-08 21:11:53 -0600

    feat:add validation to create account

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,19 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { AuthController } from "../controllers/AuthController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router: Router = Router();
 
-router.post("/create-account", AuthController.createAccount);
+router.post(
+  "/create-account",
+  body("name").notEmpty().withMessage("El nombre es obligatorio"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("La contraseña es muy corta, mínimo 8 caracteres"),
+  body("email").isEmail().withMessage("El correo no es válido"),
+  handleInputErrors,
+  AuthController.createAccount
+);
 
 export default router;
