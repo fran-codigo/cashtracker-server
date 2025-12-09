aeced72994a5d8e6000648babee03329c4a47688
Franco <franco_tigres18@hotmail.com>
2025-12-08 21:17:12 -0600

    feat:store user

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,7 +1,17 @@
 import type { Request, Response } from "express";
+import User from "../models/User";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
-    res.json("Creando cuenta");
+    try {
+      const user = new User(req.body);
+
+      await user.save();
+
+      res.json("Cuenta creada correctamente");
+    } catch (error) {
+      // console.log(error);
+      res.status(500).json({ error: "Hubo un error" });
+    }
   };
 }
