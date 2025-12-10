3753484e8a28216141cb74c99c49ca903379d535
Franco <franco_tigres18@hotmail.com>
2025-12-09 21:25:04 -0600

    feat:rate limit add

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { body } from "express-validator";
 import { AuthController } from "../controllers/AuthController";
 import { handleInputErrors } from "../middleware/validation";
+import { limiter } from "../config/limiter";
 
 const router: Router = Router();
 
@@ -18,6 +19,7 @@ router.post(
 
 router.post(
   "/confirm-account",
+  limiter,
   body("token")
     .notEmpty()
     .isLength({ min: 6, max: 6 })
